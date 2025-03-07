b9b3c5246258f503a769cde2fa5a74c755246024
qkralthf0115 <qkralthf0115@naver.com>
2025-03-07 15:42:54 +0900

    💄 add spiral design elements

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,6 +10,7 @@ const Container = styled.div`
   height: 100vh;
   position: relative;
   font-family: Arial, sans-serif;
+  overflow: hidden;
 
   &::before {
     content: "I ❤️ JH ";
@@ -53,21 +54,27 @@ const Board = styled.div`
 
 const Notebooks = styled.div`
   display: flex;
+  width: 100%;
   justify-content: space-around;
   width: 100%;
+  height: 100%;
   gap: 20px;
 `;
+interface NotebookProps {
+  boxWidth?: string;
+}
 
-const Notebook = styled.div`
+const Notebook = styled.div<NotebookProps>`
   background-color: white;
-  width: 100%;
+  width: ${(props) => (props.boxWidth ? props.boxWidth : "100%")};
   padding: 10px;
   display: flex;
-  flex-direction: column
+  flex-direction: column;
   align-items: center;
   border-radius: 20px;
   min-height: 200px;
   border: 2px solid #c7c7c7;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
 `;
 
 const Title = styled.h2`
@@ -75,6 +82,7 @@ const Title = styled.h2`
   text-align: center;
   font-size: 40px;
   font-family: "Courier", cursive, sans-serif;
+  margin: 0 0 20px 0;
 `;
 
 const Image = styled.img`
@@ -88,20 +96,46 @@ const Image = styled.img`
 const Text = styled.p`
   color: #007bff;
   text-align: center;
-  font-size: 16px;
+  font-weight: bold;
+  font-family: "Courier", cursive, sans-serif;
+`;
+
+const SpiralBinding = styled.div`
+  position: absolute;
+  left: 33%;
+  height: 65%;
+  width: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  z-index: 2;
+`;
+
+const SpiralRing = styled.div`
+  width: 52px;
+  height: 12px;
+  background: linear-gradient(to right, #c7c7c7, #ffffff);
+  border: 2px solid #c7c7c7;
+  border-radius: 20px;
 `;
 
 const App: React.FC = () => {
   return (
     <Container>
       <Board>
-        <Title>JAEHEE</Title>
+        <Title>JH</Title>
         <Notebooks>
-          <Notebook>
+          <Notebook boxWidth="40%">
             <Image src={profile} alt="profile" />
-            <Text>NCT WISH 재희</Text>
+            <Text>JH</Text>
             <Text>最高のボーカルになりたい</Text>
           </Notebook>
+          <SpiralBinding>
+            {[...Array(8)].map((_, index) => (
+              <SpiralRing key={index} />
+            ))}
+          </SpiralBinding>
           <Notebook></Notebook>
         </Notebooks>
       </Board>
