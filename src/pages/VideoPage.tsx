6cb19219f1234cb5101110b7c3471737817d3264
qkralthf0115 <qkralthf0115@naver.com>
2025-03-13 16:30:17 +0900

    💄 update VideoPage layout and add video content

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -3,24 +3,25 @@ import Layout from "./MainLayout";
 import styled from "styled-components";
 
 const VideoContent = styled.div`
-  padding: 20px;
   width: 100%;
+  overflow-y: auto;
 `;
 
 const VideoContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
+  padding: 20px;
 `;
 
 const VideoTitle = styled.h2`
   color: #337ab7;
-  margin-bottom: 15px;
+  margin: 0;
 `;
 
 const VideoDescription = styled.p`
   color: #666;
-  margin-bottom: 10px;
+  margin: 0;
 `;
 
 const Video: React.FC = () => {
@@ -28,11 +29,34 @@ const Video: React.FC = () => {
     <Layout>
       <VideoContent>
         <VideoContainer>
-          <VideoTitle>My Videos</VideoTitle>
-          <VideoDescription>
-            제가 부른 노래와 공연 영상을 모아둔 공간입니다.
-          </VideoDescription>
-          {/* 여기에 실제 비디오 컨텐츠를 추가할 수 있습니다 */}
+          <VideoTitle>Video 1</VideoTitle>
+          <VideoDescription>description of the video</VideoDescription>
+          <iframe
+            width="320"
+            height="180"
+            src="https://www.youtube.com/embed/1w7OgIMMRc4"
+            title="YouTube video player"
+          ></iframe>
+        </VideoContainer>
+        <VideoContainer>
+          <VideoTitle>Video 1</VideoTitle>
+          <VideoDescription>description of the video</VideoDescription>
+          <iframe
+            width="320"
+            height="180"
+            src="https://www.youtube.com/embed/1w7OgIMMRc4"
+            title="YouTube video player"
+          ></iframe>
+        </VideoContainer>
+        <VideoContainer>
+          <VideoTitle>Video 1</VideoTitle>
+          <VideoDescription>description of the video</VideoDescription>
+          <iframe
+            width="320"
+            height="180"
+            src="https://www.youtube.com/embed/1w7OgIMMRc4"
+            title="YouTube video player"
+          ></iframe>
         </VideoContainer>
       </VideoContent>
     </Layout>
