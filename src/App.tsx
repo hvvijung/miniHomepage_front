b7bfaba36d2d48c95dda67eb2288a7a72f2defcf
qkralthf0115 <qkralthf0115@naver.com>
2025-03-16 00:22:13 +0900

    ✨ route diary, visitors, gallery page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import VideoPage from "./pages/VideoPage";
+import DiaryPage from "./pages/DiaryPage";
+import GalleryPage from "./pages/GalleryPage";
+import VisitorsPage from "./pages/VisitorsPage";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +14,9 @@ const App: React.FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/video" element={<VideoPage />} />
+        <Route path="/diary" element={<DiaryPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/visitors" element={<VisitorsPage />} />
       </Routes>
     </Router>
   );
